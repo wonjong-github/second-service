97dbf635bcacc08673124b13dd4352074511e852
wonjong <david8318@naver.com>
2023-12-20 00:31:39 +0900

    Initial commit

---
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -0,0 +1,35 @@
+package com.example.secondservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+
+
+@RestController
+@RequestMapping("/second-service")
+@Slf4j
+public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome to the second service";
+    }  
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        log.info(header);
+        return "Hello world new Second service";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "First service";
+    }
+}
